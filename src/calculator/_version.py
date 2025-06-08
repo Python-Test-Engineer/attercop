724fbea369f1e47a955f466807c9d103c3eda38d
Craig West <iwswordpress@gmail.com>
2025-06-08 17:46:17 +0100

    feat: add ruff to project.toml

--- src/calculator/_version.py
+++ src/calculator/_version.py
@@ -5,7 +5,8 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
@@ -16,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev15+gd8fc41f.d20250608"
-__version_tuple__ = version_tuple = (0, 1, "dev15", "gd8fc41f.d20250608")
+__version__ = version = "1.0.1.dev12+g863b223.d20250608"
+__version_tuple__ = version_tuple = (1, 0, 1, "dev12", "g863b223.d20250608")
