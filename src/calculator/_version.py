060a50fc7c265ca5c62d7985ed599c7eb1fee3ca
Craig West <iwswordpress@gmail.com>
2025-06-08 19:02:30 +0100

    feat: use Python version and was 1.1.0

--- src/calculator/_version.py
+++ src/calculator/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "1.0.1.dev12+g863b223.d20250608"
-__version_tuple__ = version_tuple = (1, 0, 1, "dev12", "g863b223.d20250608")
+__version__ = version = "1.1.1.dev1+g1c84154.d20250608"
+__version_tuple__ = version_tuple = (1, 1, 1, "dev1", "g1c84154.d20250608")
