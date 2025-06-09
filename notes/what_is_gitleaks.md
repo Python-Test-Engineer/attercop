ec3320731d238ef13e9133fbfb1c14478801c623
Craig West <iwswordpress@gmail.com>
2025-06-09 08:38:53 +0100

    chore: add notes

---
+++ notes/what_is_gitleaks.md
@@ -0,0 +1,5 @@
+# What is GitLeaks?
+
+Gitleaks is a tool for detecting secrets like passwords, API keys, and tokens in git repos, files, and whatever else you wanna throw at it via stdin.
+
+To learn more about how the detection engine works check out this blog: [Regex is (almost) all you need.](https://lookingatcomputer.substack.com/p/regex-is-almost-all-you-need)
--- src/calculator/__init__.py
+++ src/calculator/__init__.py
@@ -3,6 +3,6 @@
 from .calculator import Calculator
 
 __version__ = "1"
-# this is not part of workflow as version number is hard coded in pyproject.toml
+# this is not part of workflow as version number is hard coded in pyproject.toml and not passed from here. Version number in pytproject.toml is used for the workflow and must be the latest version.
 
 __all__ = ["Calculator"]
