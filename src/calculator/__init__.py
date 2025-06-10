3ac51160464beb2264df4cb2e2b7a7cb54bd4698
Craig West <iwswordpress@gmail.com>
2025-06-10 12:28:34 +0100

    feat: 1.35.0 direct CHANGELOG.md read - remove __version__ etc

--- src/calculator/__init__.py
+++ src/calculator/__init__.py
@@ -2,7 +2,7 @@
 
 from .calculator import Calculator
 
-__version__ = "1"
+# __version__ = "1"
 # this is not part of workflow as version number is hard coded in pyproject.toml and not passed from here. Version number in pytproject.toml is used for the workflow and must be the latest version.
 
 __all__ = ["Calculator"]
