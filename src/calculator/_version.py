3c72af8508a12d7143fbc14e6ccc1cce5d64cec9
Craig West <iwswordpress@gmail.com>
2025-06-26 17:52:22 +0100

    chore: organise pre-commit

--- src/calculator/_version.py
+++ src/calculator/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "2.0.1.dev46+g1a9e313.d20250626"
-__version_tuple__ = version_tuple = (2, 0, 1, "dev46", "g1a9e313.d20250626")
+__version__ = version = "2.0.1.dev47+g6584cff.d20250626"
+__version_tuple__ = version_tuple = (2, 0, 1, "dev47", "g6584cff.d20250626")
